c5e5f7df81b059cf38859a054bb923a1182a188d
ebrewe <ericdbrewer@gmail.com>
2016-01-14 22:23:33 +0700

    Built out a small form with refs

---
+++ src/components/NameField.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+
+var NameField = React.createClass({
+    getInitialState: function(){
+       return {value: ""};  
+    },
+    clear: function(){
+      this.setState({value:""});  
+    },
+    onChange: function(e){
+       this.setState({value: e.target.value});  
+    },
+    render: function(){
+        return (
+            <input className="form-control" placeholder={this.props.type +" Name"} onChange={this.onChange} value={this.state.value} />
+        );
+    }
+});
+
+module.exports = NameField;
